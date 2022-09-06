461942f9c150846d43f08219385ee4b6a7c2571c
jh11240 <jh11240@naver.com>
2022-09-06 15:15:19 +0900

    Update 백준2805(나무자르기).cpp

--- binarySearch/백준2805(나무자르기).cpp
+++ binarySearch/백준2805(나무자르기).cpp
@@ -30,19 +30,36 @@ long long cut(long long middle) {
 void solution() {
 	//조건을 만족하는 최대값 low, 조건을 불만족하는 최소값 high로 설정
 	long long low = 0, high = inputArr[N-1];
-	//low와 high가 1차이 날 때까지
-	while (low+1 < high) {
-		//mid값은 low와 high더한값이 int값 넘어갈 수도 있으므로 long long으로 선언
-		long long mid = (low + high) / 2;
 
-		//mid값으로 잘랐을 때 타깃 길이인 M보다 같거나 크다면 low값을 mid로 변경해줌
-		if (cut(mid) >= M) low = mid;
-		//작다면 high값을 mid로 변경해주어 범위를 바꿔준다.
+	//1번 방법
+
+	////low와 high가 1차이 날 때까지
+	//while (low+1 < high) {
+	//	//mid값은 low와 high더한값이 int값 넘어갈 수도 있으므로 long long으로 선언
+	//	long long mid = (low + high) / 2;
+
+	//	//mid값으로 잘랐을 때 타깃 길이인 M보다 같거나 크다면 low값을 mid로 변경해줌
+	//	if (cut(mid) >= M) low = mid;
+	//	//작다면 high값을 mid로 변경해주어 범위를 바꿔준다.
+	//	else
+	//		high = mid;
+	//}
+	//cout<<low;
+
+	//2번 방법
+
+	long long mid = 0;
+	while (low <= high) {
+		mid = (low + high) / 2;
+
+		if (cut(mid) >= M) low = mid+1;
+
 		else
-			high = mid;
+			high = mid-1;
 	}
-	//답을 만족하는 최대값인 low을 출력
-	cout << low;
+
+	cout << (low + high)/2;
+	//cout<<high; 둘 다됨 어차피 cut(mid)=M다음 단계에선 cut(mid)<M이 되므로 high =mid-1이 실행되고 high값이 답이나옴.
 
 }
 
