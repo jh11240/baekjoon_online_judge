857c81b0b8017bcb8480b13d591c17705b473293
jh11240 <jh11240@naver.com>
2022-12-05 21:57:22 +0900

    Update 백준1039(교환).cpp

--- BFS/백준1039(교환).cpp
+++ BFS/백준1039(교환).cpp
@@ -6,17 +6,13 @@
 using namespace std;
 //N, K , K번 바꿨을때 최대수
 string N;
-int K=0,maxNum = -1;
+int K = 0, maxNum = -1;
 set<string> visited;
 void Input() {
 	cin >> N >> K;
 }
 
 void Solution() {
-	if (N.length() < K) {
-		cout << -1;
-		return;
-	}
 	queue<string> q;
 	q.push(N);
 	while (!q.empty()) {
@@ -25,13 +21,12 @@ void Solution() {
 		visited.clear();
 		for (int i = 0; i < qSize; i++) {
 			string cur = q.front();
-			//visited.insert(cur);
 			q.pop();
-			for (int j = 0; j < N.length()-1; j++) {
+			for (int j = 0; j < N.length() - 1; j++) {
 				//i이전 값
-				for (int k = j+1; k < N.length(); k++) {
+				for (int k = j + 1; k < N.length(); k++) {
 					string tmp = cur;
-					if (tmp[k] == 0) continue;
+					if (j==0 && tmp[k] == '0') continue;
 					swap(tmp[j], tmp[k]);
 					if (visited.find(tmp) == visited.end()) {
 						visited.insert(tmp);
@@ -41,7 +36,7 @@ void Solution() {
 			}
 		}
 		if (K == 0) {
-			while(!q.empty()){
+			while (!q.empty()) {
 				int cur = stoi(q.front());
 				maxNum = maxNum > cur ? maxNum : cur;
 				q.pop();
@@ -49,8 +44,8 @@ void Solution() {
 			cout << maxNum;
 			return;
 		}
-
 	}
+	cout << -1;
 }
 int main() {
 	Input();
