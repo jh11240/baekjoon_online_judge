29ffa09cbb1dd99f467a84e89b33518329c920b1
jh11240 <jh11240@naver.com>
2022-12-07 23:01:13 +0900

    Update 백준1759(암호 만들기).cpp

--- BackTracking/백준1759(암호 만들기).cpp
+++ BackTracking/백준1759(암호 만들기).cpp
@@ -3,8 +3,9 @@
 #include<algorithm>
 using namespace std;
 
-int L=0, C = 0;
+int L = 0, C = 0;
 char Letters[16];
+char vowels[5] = { 'a','e','o','i','u' };
 vector<char>Ans;
 vector<string>printAns;
 
@@ -22,23 +23,29 @@ void Input() {
 /// 끝나고 Ans에서 해당 값을 빼줌으로서 노드 방문 초기화
 /// </summary>
 /// <param name="Index(cur)"></param>
-void DFS(int cur,int consonant,int vowel) {
+void DFS(int cur, int consonant, int vowel) {
 	//index가 끝에 다다르면 return한다.
 	if (cur == C) return;
 	//Ans.size가 L일때 출력하면 안되는 게, 만약 cur이 c-1일때 마지막값을 푸시하고 DFS(cur+1)을 실행하면
 	// cur+1은 c이므로 return 당해서 출력 하는 부분이 호출이 안됨.
-	if (Ans.size() == L-1) {
-		string tmp = "";
-		for (auto iter = Ans.begin(); iter != Ans.end(); ++iter) {
-			tmp += *iter;
+	if (Ans.size() == L - 1) {
+		if (consonant >= 2 && vowel >= 1) {
+			string tmp = "";
+			for (auto iter = Ans.begin(); iter != Ans.end(); ++iter) {
+				tmp += *iter;
+			}
+			printAns.push_back(tmp + Letters[cur]);
 		}
-		printAns.push_back(tmp+Letters[cur]);
+		
 	}
 
-	DFS(cur + 1,consonant,vowel);
+	DFS(cur + 1, consonant, vowel);
 	Ans.push_back(Letters[cur]);
-
-	DFS(cur + 1);
+	if(Letters[cur]=='a'|| Letters[cur] == 'e'|| Letters[cur] == 'i'|| Letters[cur] == 'o'|| Letters[cur] == 'u')
+		DFS(cur + 1,consonant,vowel+1);
+	else {
+		DFS(cur + 1, consonant + 1, vowel);
+	}
 	for (auto iter = Ans.begin(); iter != Ans.end(); ++iter) {
 		if (*iter == Letters[cur]) {
 			Ans.erase(iter);
@@ -48,7 +55,7 @@ void DFS(int cur,int consonant,int vowel) {
 }
 
 void Solution() {
-	DFS(0,0,0);
+	DFS(0, 0, 0);
 	sort(printAns.begin(), printAns.end());
 	for (auto iter = printAns.begin(); iter != printAns.end(); ++iter) {
 		cout << *iter << '\n';
