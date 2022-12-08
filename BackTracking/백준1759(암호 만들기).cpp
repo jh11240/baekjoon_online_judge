b4a95fa6fafecd7bcafd8b119281630b8e9f5a6d
JH Seo <jh11240@naver.com>
2022-12-08 15:02:00 +0900

    Update 백준1759(암호 만들기).cpp

--- BackTracking/백준1759(암호 만들기).cpp
+++ BackTracking/백준1759(암호 만들기).cpp
@@ -29,23 +29,45 @@ void DFS(int cur, int consonant, int vowel) {
 	//Ans.size가 L일때 출력하면 안되는 게, 만약 cur이 c-1일때 마지막값을 푸시하고 DFS(cur+1)을 실행하면
 	// cur+1은 c이므로 return 당해서 출력 하는 부분이 호출이 안됨.
 	if (Ans.size() == L - 1) {
-		if (consonant >= 2 && vowel >= 1) {
+		//cur인덱스의 글자가 모음이라면
+		if (Letters[cur] == 'a' || Letters[cur] == 'e' || Letters[cur] == 'i' || Letters[cur] == 'o' || Letters[cur] == 'u') {
+			// 지금까지 자음이 2개 이상이여야함
+			if (consonant >= 2) {
 			string tmp = "";
+			//char문자들 합쳐준 후
 			for (auto iter = Ans.begin(); iter != Ans.end(); ++iter) {
 				tmp += *iter;
 			}
+			//cur인덱스에 해당하는 문자까지 추가해서 답벡터에 넣어줌
 			printAns.push_back(tmp + Letters[cur]);
+			}
+
 		}
-		
+		//cur인덱스의 글자가 자음이라면 지금까지 자음,모음이 1개이상씩 있어야함
+		else if (consonant >= 1 && vowel >= 1) {
+				string tmp = "";
+				for (auto iter = Ans.begin(); iter != Ans.end(); ++iter) {
+					tmp += *iter;
+				}
+				//cur인덱스에 해당하는 문자까지 추가해서 답벡터에 넣어줌
+				printAns.push_back(tmp + Letters[cur]);
+		}
+		//위의 조건에 해당 안되면 패스
 	}
 
+	//cur인덱스의 문자 패스하고 dfs
 	DFS(cur + 1, consonant, vowel);
+
+	//cur인덱스의 문자 포함해주고 
 	Ans.push_back(Letters[cur]);
-	if(Letters[cur]=='a'|| Letters[cur] == 'e'|| Letters[cur] == 'i'|| Letters[cur] == 'o'|| Letters[cur] == 'u')
-		DFS(cur + 1,consonant,vowel+1);
+	if (Letters[cur] == 'a' || Letters[cur] == 'e' || Letters[cur] == 'i' || Letters[cur] == 'o' || Letters[cur] == 'u')
+		//만약 문자가 모음이면 vowel+1해주고 dfs
+		DFS(cur + 1, consonant, vowel + 1);
 	else {
+		//자음이면 consonant+1
 		DFS(cur + 1, consonant + 1, vowel);
 	}
+	//매번 dfs가 끝나고 돌아오면 다시 이노드를 방문할 수 있으므로 해당 cur인덱스의 문자를 제거해준다. 
 	for (auto iter = Ans.begin(); iter != Ans.end(); ++iter) {
 		if (*iter == Letters[cur]) {
 			Ans.erase(iter);
