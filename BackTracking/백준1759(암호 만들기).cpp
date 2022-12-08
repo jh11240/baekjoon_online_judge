0d936b56eeae052fb2508e0b61f6fb85bc837425
JH Seo <jh11240@naver.com>
2022-12-08 15:49:29 +0900

    Update 백준1759(암호 만들기).cpp

--- BackTracking/백준1759(암호 만들기).cpp
+++ BackTracking/백준1759(암호 만들기).cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 int L = 0, C = 0;
 char Letters[16];
-char vowels[5] = { 'a','e','o','i','u' };
+//각 dfs에서 사용할 char문자 담는 벡터
 vector<char>Ans;
+//4글자짜리가 나온다면 저장해서 마지막에 sort하는 벡터
 vector<string>printAns;
 
 void Input() {
@@ -23,7 +24,7 @@ void Input() {
 /// 끝나고 Ans에서 해당 값을 빼줌으로서 노드 방문 초기화
 /// </summary>
 /// <param name="Index(cur)"></param>
-void DFS(int cur, int consonant, int vowel) {
+void Backtracking(int cur, int consonant, int vowel) {
 	//index가 끝에 다다르면 return한다.
 	if (cur == C) return;
 	//Ans.size가 L일때 출력하면 안되는 게, 만약 cur이 c-1일때 마지막값을 푸시하고 DFS(cur+1)을 실행하면
@@ -56,16 +57,16 @@ void DFS(int cur, int consonant, int vowel) {
 	}
 
 	//cur인덱스의 문자 패스하고 dfs
-	DFS(cur + 1, consonant, vowel);
+	Backtracking(cur + 1, consonant, vowel);
 
 	//cur인덱스의 문자 포함해주고 
 	Ans.push_back(Letters[cur]);
 	if (Letters[cur] == 'a' || Letters[cur] == 'e' || Letters[cur] == 'i' || Letters[cur] == 'o' || Letters[cur] == 'u')
 		//만약 문자가 모음이면 vowel+1해주고 dfs
-		DFS(cur + 1, consonant, vowel + 1);
+		Backtracking(cur + 1, consonant, vowel + 1);
 	else {
 		//자음이면 consonant+1
-		DFS(cur + 1, consonant + 1, vowel);
+		Backtracking(cur + 1, consonant + 1, vowel);
 	}
 	//매번 dfs가 끝나고 돌아오면 다시 이노드를 방문할 수 있으므로 해당 cur인덱스의 문자를 제거해준다. 
 	for (auto iter = Ans.begin(); iter != Ans.end(); ++iter) {
@@ -77,7 +78,7 @@ void DFS(int cur, int consonant, int vowel) {
 }
 
 void Solution() {
-	DFS(0, 0, 0);
+	Backtracking(0, 0, 0);
 	sort(printAns.begin(), printAns.end());
 	for (auto iter = printAns.begin(); iter != printAns.end(); ++iter) {
 		cout << *iter << '\n';
