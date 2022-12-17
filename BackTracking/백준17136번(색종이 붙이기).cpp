8acb0a1b7337b78db34e9b6829dd2e92c9be2cdc
jh11240 <jh11240@naver.com>
2022-12-17 16:45:33 +0900

    Update 백준17136번(색종이 붙이기).cpp

--- BackTracking/백준17136번(색종이 붙이기).cpp
+++ BackTracking/백준17136번(색종이 붙이기).cpp
@@ -12,8 +12,8 @@ struct oneInfo {
 
 vector<oneInfo> ones;
 //만약 답이없을때는 -1 출력해야되는데 minAns를 출력하게하므로 100출력
-bool didFinished=false;
-int minAns=100;
+bool didFinished = false;
+int minAns = 100;
 bool covered[10][10];
 int paperAmount[5] = { 5,5,5,5,5 };
 
@@ -21,15 +21,15 @@ void Input() {
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 10; j++) {
 			cin >> paper[i][j];
-			if (paper[i][j] == 1) ones.push_back({ i,j});
+			if (paper[i][j] == 1) ones.push_back({ i,j });
 		}
 	}
 }
 
 //그냥 단순히 h,w에서 num만큼 다 false로 해버리면 예를들어 num이 5일때 1,2가 0일때 방금 조사안한값들도 다 false로 바꿔놓음
 void returnCoveredToFalse(int num, int height, int width) {
-	for (int i = height; i < num+height; i++) {
-		for (int j = width; j < num+width; j++) {
+	for (int i = height; i < num + height; i++) {
+		for (int j = width; j < num + width; j++) {
 			if (covered[i][j])
 				covered[i][j] = false;
 			else
@@ -45,11 +45,12 @@ void returnCoveredToFalse(int num, int height, int width) {
 /// <param name="좌표 행(height)"></param>
 /// <param name="좌표 열(width)"></param>
 /// <returns></returns>
-bool checkIfZeroCovered(int num,int height,int width) {
-	for (int i = height; i <height+ num; i++) {
-		for (int j = width; j <width+ num; j++) {
+bool checkIfZeroCovered(int num, int height, int width) {
+	for (int i = height; i < height + num; i++) {
+		for (int j = width; j < width + num; j++) {
 			//0을 만나면 true리턴
-			if (paper[i][j] == 0) {
+			//covered가 false일때만 true로 변경해야함 아니면 line92 안에서 백트래킹할때 그안에 이미 방문했음에도 다시 변경해버림
+			if (paper[i][j] == 0|| covered[i][j]==true) {
 				return true;
 			}
 			//paper가 1이라면 continue
@@ -78,23 +79,23 @@ int HowMuchPaperUsed() {
 void Bactracking(vector<oneInfo>::iterator oneOffset) {
 	if (oneOffset == ones.end()) {
 		didFinished = true;
-		minAns = minAns< HowMuchPaperUsed()? minAns:HowMuchPaperUsed();
+		minAns = minAns < HowMuchPaperUsed() ? minAns : HowMuchPaperUsed();
 		return;
 	}
 	//만약 다음 1의 자리가 색종이 덮여있다면 다음 1의자리로 패스
 	if (covered[oneOffset->height][oneOffset->width]) {
-		Bactracking(oneOffset+1);
+		Bactracking(oneOffset + 1);
 		//1의 자리가 덮여있다면 밑의 과정 처리 안해도되므로 백트래킹 끝나고 바로 return
 		return;
 	}
-	for (int i = 5; i >0; i--) {
+	for (int i = 5; i > 0; i--) {
 		//0이 색종이로 커버가 된다면 continue , i값 더했을때 범위벗어나면 continue해줘야됨(범위도 더한값이 10까지 허용해줘야 끝값 체크 가능)
-		if ((oneOffset->height + i>10 || oneOffset->width+i>10) ||checkIfZeroCovered(i, oneOffset->height, oneOffset->width)) 
+		if ((oneOffset->height + i > 10 || oneOffset->width + i > 10) || checkIfZeroCovered(i, oneOffset->height, oneOffset->width))
 			continue;
 		//0이 안된다면 이미 해당 구역의 1들은 다 covered가 true 체크되었을 것.
 		else {
 			//갯수 -1 해줌
-			paperAmount[i-1]--;
+			paperAmount[i - 1]--;
 			//갯수가 음수가 나온다면 이번 재귀에서 바꾼 covered값들 초기화 
 			if (paperAmount[i - 1] < 0) {
 				returnCoveredToFalse(i, oneOffset->height, oneOffset->width);
@@ -107,9 +108,6 @@ void Bactracking(vector<oneInfo>::iterator oneOffset) {
 				returnCoveredToFalse(i, oneOffset->height, oneOffset->width);
 				paperAmount[i - 1]++;
 				continue;
-			}
-			if (HowMuchPaperUsed() == 6) {
-				cout << "66";
 			}
 			Bactracking(oneOffset + 1);
 			//백트래킹끝나고 초기화
